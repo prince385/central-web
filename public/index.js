5f5d47ea3536bfec21dd172d3dc85dd0b25eb5b0
prince385 <princejebad0@gmail.com>
2023-08-17 19:06:04 +0530

    patched

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Apr 01, 2023 12:00:00").getTime();
+var countDownDate = new Date("Aug 11, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
